5c562905523db75347be0582f1a77a6053a65452
hybras <24651269+hybras@users.noreply.github.com>
2020-11-18 14:07:33 -0500

    Move aliases into main brew plugin

--- brew/brew.plugin.zsh
+++ brew/brew.plugin.zsh
@@ -1,7 +1,18 @@
 brew bundle
 
-source "${0:h}/aliases.zsh"
-
 fpath+=( "${0:h}/functions" )
-
 autoload -Uz exa viddl songdl notify
+
+alias jsh='jshell'
+alias python='python3'
+alias py='python3'
+alias pip='pip3'
+alias nano='nano -a'
+alias ytdl='youtube-dl'
+alias cat='bat'
+#alias notify='notify-send'
+alias proton='sudo protonvpn'
+alias ps='procs'
+alias vi='nvim'
+
+alias l1='exa -1'
