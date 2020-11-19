4aa5ab4214bb42024c0b9cdfe19dd367036396b4
hybras <24651269+hybras@users.noreply.github.com>
2020-11-18 22:49:20 -0500

    fix grace alias

--- grace/grace.plugin.zsh
+++ grace/grace.plugin.zsh
@@ -1,3 +1,3 @@
 export umd_pass_file="${0:h}/grace/umd pass.txt"
 
-alias grace="sshpass -f ${umd_pass_file} vchari@grace-new.umd.edu"
+alias grace="sshpass -f \"${umd_pass_file}\" ssh vchari@grace-new.umd.edu"
