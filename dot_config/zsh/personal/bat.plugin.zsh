783d91606ae77aa7047829cc14dce231ff356b15
hybras <24651269+hybras@users.noreply.github.com>
2022-07-31 23:06:26 -0400

    bat plugin syncs bat theme with macos dark mode

---
+++ dot_config/zsh/personal/bat.plugin.zsh
@@ -0,0 +1,6 @@
+function bat_theme() {
+  export BAT_THEME=$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd bat_theme
