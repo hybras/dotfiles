4c42f6e81a22d44160ff8d087bd72bc7ae51e80c
hybras <24651269+hybras@users.noreply.github.com>
2025-05-17 08:05:26 -0400

    Dont specify brewfile path

--- run_once_after_install-packages-darwin.sh
+++ run_once_after_install-packages-darwin.sh
@@ -2,7 +2,6 @@
 
 curl -fsSL https://github.com/Homebrew/brew/releases/download/4.5.2/Homebrew-4.5.2.pkg -o ~/Downloads/Homebrew-4.5.2.pkg
 sudo installer -pkg ~/Downloads/Homebrew-4.5.2.pkg -target /
-eval `/opt/homebrew/bin/brew shellenv`
 
-brew bundle install --quiet --file ~/.config/homebrew/Brewfile
+brew bundle install --quiet --file --global
 
