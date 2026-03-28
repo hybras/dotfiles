4bba2129e0f9b0eff6e7a4d4415003bae9e480c2
hybras <24651269+hybras@users.noreply.github.com>
2026-03-28 11:42:58 -0400

    Not verbose

--- dot_config/zsh/personal/bat.plugin.zsh
+++ dot_config/zsh/personal/bat.plugin.zsh
@@ -2,11 +2,11 @@ function bat_theme() {
   if defaults read -globalDomain AppleInterfaceStyle &> /dev/null; then
     unset  BAT_THEME
     unset  MCFLY_LIGHT
-    ln -sfv ~/.config/alacritty/themes/default.toml ~/.config/alacritty/themes/autoswitch.toml
+    ln -sf ~/.config/alacritty/themes/default.toml ~/.config/alacritty/themes/autoswitch.toml
   else
     export MCFLY_LIGHT=TRUE
     export BAT_THEME="Monokai Extended Light"
-    ln -sfv ~/.config/alacritty/themes/atom_one_light.toml ~/.config/alacritty/themes/autoswitch.toml
+    ln -sf ~/.config/alacritty/themes/atom_one_light.toml ~/.config/alacritty/themes/autoswitch.toml
   fi
 }
 
