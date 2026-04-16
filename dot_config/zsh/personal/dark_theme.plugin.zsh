1f3a6709dad5b893cd162846c820ea1544ea76ac
hybras <24651269+hybras@users.noreply.github.com>
2026-04-16 11:17:46 -0400

    move alacritty dark/light them autoswitch elsewheree

--- dot_config/zsh/personal/dark_theme.plugin.zsh
+++ dot_config/zsh/personal/dark_theme.plugin.zsh
@@ -2,11 +2,9 @@ function bat_theme() {
   if defaults read -globalDomain AppleInterfaceStyle &> /dev/null; then
     unset  BAT_THEME
     unset  MCFLY_LIGHT
-    ln -sf ~/.config/alacritty/themes/default.toml ~/.config/alacritty/themes/autoswitch.toml
   else
     export MCFLY_LIGHT=TRUE
     export BAT_THEME="Monokai Extended Light"
-    ln -sf ~/.config/alacritty/themes/atom_one_light.toml ~/.config/alacritty/themes/autoswitch.toml
   fi
 }
 
